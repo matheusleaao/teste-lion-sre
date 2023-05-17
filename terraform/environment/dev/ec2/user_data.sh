e248ac3d602bab1c649d7ff2a4f6c558afcaf8ad
matheusleaao <mathegiov@hotmail.com>
2023-05-17 12:57:12 -0300

    teste actions whoami

--- terraform/environment/dev/ec2/user_data.sh
+++ terraform/environment/dev/ec2/user_data.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 echo "Setup ubuntu"
-sudo apt update -y
-sudo apt install net-tools
-sudo ifconfig eth0 mtu 1350
+apt update -y
+apt install net-tools
+ifconfig eth0 mtu 1350
 echo "installing snap"
-sudo apt install snapd -y
-sudo systemctl enable --now snapd.socket
-sudo ln -s /var/lib/snapd/snap /snap
+apt install snapd -y
+systemctl enable --now snapd.socket
+ln -s /var/lib/snapd/snap /snap
 
 echo "Config amazon-ssm"
-sudo snap install amazon-ssm-agent --classic
-sudo snap start amazon-ssm-agent
+snap install amazon-ssm-agent --classic
+snap start amazon-ssm-agent
 
 echo "installing microk8s"
-sudo snap install microk8s --classic
+snap install microk8s --classic
 
 echo "Starting microk8s"
-sudo microk8s start
-sudo microk8s enable dns
+microk8s status --wait-ready
+microk8s enable dns
+
+# Create a folder
+su ubuntu -c '
+    whoami
+    cd /tmp
+    mkdir actions-runner && cd actions-runner
+    curl -o actions-runner-linux-x64-2.304.0.tar.gz -L https://github.com/actions/runner/releases/download/v2.304.0/actions-runner-linux-x64-2.304.0.tar.gz
+    echo "292e8770bdeafca135c2c06cd5426f9dda49a775568f45fcc25cc2b576afc12f  actions-runner-linux-x64-2.304.0.tar.gz" | shasum -a 256 -c
+    tar xzf ./actions-runner-linux-x64-2.304.0.tar.gz
+    ./config.sh --url https://github.com/matheusleaao/teste-lion-sre --token AK6AAEKZXHUBZZSQAIOEL4LEMT2TK
+    nohup ./run.sh  &
+'
+#o token muda, precisa ser configurado manualmente
